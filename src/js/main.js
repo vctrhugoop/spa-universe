8231f19013eb662539c50b1bacb6a43f41eb344d
vctrhugoop <vhdestroy@hotmail.com>
2023-11-29 19:59:26 -0300

    🎉 init: commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,14 @@
-import './toggleMenu';
+import './toggleMenu.js';
+import { Router } from './router.js';
+
+const router = new Router();
+
+router.add('/', '/src/pages/home.html');
+router.add('/universe', '/src/pages/universe.html');
+router.add('/explore', '/src/pages/explore.html');
+router.add(404, '/src/pages/404.html');
+
+router.handle();
+
+window.onpopstate = () => router.handle();
+window.route = () => router.route();
