264b293a51580a446b945331b08a5ef9926b87db
vctrhugoop <vhdestroy@hotmail.com>
2023-11-29 20:14:11 -0300

    🎉 init: commit

--- src/js/main.js
+++ src/js/main.js
@@ -3,10 +3,10 @@ import { Router } from './router.js';
 
 const router = new Router();
 
-router.add('/', '/src/pages/home.html');
-router.add('/universe', '/src/pages/universe.html');
-router.add('/explore', '/src/pages/explore.html');
-router.add(404, '/src/pages/404.html');
+router.add('/', '/pages/home.html');
+router.add('/universe', '/pages/universe.html');
+router.add('/explore', '/pages/explore.html');
+router.add(404, '/pages/404.html');
 
 router.handle();
 
